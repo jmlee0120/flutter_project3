fee59bfb0cdf1855ce8fa1b10fde530797a94ea1
jmlee0120 <hminjmin04@gmail.com>
2025-03-21 15:28:37 +0900

    수정사항: 하단바 터치효과 수정

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,6 @@
 // lib/screens/main_screen.dart
 import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart'; // 이 패키지 추가 필요
 import '../constants/app_constants.dart';
 import '../services/notification_service.dart';
 import '../widgets/notification_badge.dart';
@@ -26,22 +27,6 @@ class _MainScreenState extends State<MainScreen> {
     const MyPageScreen(),
   ];
 
-  // 하단 탭 항목 목록
-  final List<BottomNavigationBarItem> _items = [
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.forum),
-      label: AppConstants.BOARD,
-    ),
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.people),
-      label: AppConstants.MEETUP,
-    ),
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.person),
-      label: AppConstants.MYPAGE,
-    ),
-  ];
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -55,7 +40,7 @@ class _MainScreenState extends State<MainScreen> {
       appBar: AppBar(
         title: Image.asset(
           'assets/icons/macaron_appbarlogo.png',
-          height: 25,  // Adjust the height as needed
+          height: 25,
           fit: BoxFit.contain,
         ),
         centerTitle: false,
@@ -88,14 +73,25 @@ class _MainScreenState extends State<MainScreen> {
         ],
       ),
       body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: _items,
-        currentIndex: _selectedIndex,
+
+      // 수정된 하단바: 라운딩 효과 유지하면서 선택된 아이템은 아이콘과 텍스트 색상만 변경
+      bottomNavigationBar: ConvexAppBar(
+        style: TabStyle.react, // 라운딩 효과를 위해 react 스타일 사용
+        backgroundColor: Colors.white,
+        color: Colors.grey, // 선택되지 않은 아이템 색상
+        activeColor: Colors.blue, // 선택된 아이템 색상
+        elevation: 4, // 그림자 효과
+        items: [
+          TabItem(icon: Icons.forum, title: AppConstants.BOARD),
+          TabItem(icon: Icons.people, title: AppConstants.MEETUP),
+          TabItem(icon: Icons.person, title: AppConstants.MYPAGE),
+        ],
+        initialActiveIndex: _selectedIndex,
         onTap: _onItemTapped,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        showUnselectedLabels: true,
-        type: BottomNavigationBarType.fixed,
+        height: 60, // 하단바 높이 조정
+        top: -3, // 얼마나 위로 튀어나오게 할지 (음수 값)
+        curveSize: 90, // 곡률 크기
+        curve: Curves.easeInOut, // 부드러운 곡선 효과 추가
       ),
     );
   }
